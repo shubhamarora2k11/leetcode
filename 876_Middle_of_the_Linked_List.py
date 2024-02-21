11812d7c543d925c5547ed274daf1f429f897640
shubhamarora2k11 <shubhamarora2k11@users.noreply.github.com>
2024-02-21 23:13:55 +0530

    Create 876_Middle_of_the_Linked_List.py

---
+++ 876_Middle_of_the_Linked_List.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        x = head
+        y = head
+        while(y and y.next):
+            x = x.next
+            y = y.next.next
+        return x
+        
