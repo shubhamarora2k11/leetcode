d2480b75b027f5dd88f6f838c9496c032dd9519b
shubhamarora2k11 <shubhamarora2k11@users.noreply.github.com>
2024-02-21 22:44:08 +0530

    Create 412_Fizz_Buzz.py

---
+++ 412_Fizz_Buzz.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        temp = []
+        for i in range(1,n+1):
+            if i%15==0: temp.append('FizzBuzz')
+            elif i%3==0: temp.append('Fizz')
+            elif i%5==0: temp.append('Buzz')
+            else: temp.append(str(i))
+        return temp
+        
